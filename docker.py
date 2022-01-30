c046cb99fc1005e7c06bf0105168c6058de7d6da
JaimeMadridAlonso <36418914+JaimeMadridAlonso@users.noreply.github.com>
2022-01-30 23:08:03 +0100

    Update docker.py

--- docker.py
+++ docker.py
@@ -5,11 +5,11 @@ import os
 
 if len(sys.argv)==2:
   if sys.argv[1]=='1':
-    os.system('docker-compose -f docker.compose.yml up --build')
+    os.system('docker-compose -f docker-compose.yml up --build')
   elif sys.argv[1]=='2':
-    os.system('docker-compose -f docker.compose2.yml up --build')
+    os.system('docker-compose -f docker-compose2.yml up --build')
   elif sys.argv[1]=='3':
-    os.system('docker-compose -f docker.compose3.yml up --build')
+    os.system('docker-compose -f docker-compose3.yml up --build')
   else:
     print("Elige un valor del 1-3")
 else:
