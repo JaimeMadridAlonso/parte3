ad79b439fc6b2b286c07f09c51f03fca030bcad8
JaimeMadridAlonso <36418914+JaimeMadridAlonso@users.noreply.github.com>
2022-01-30 23:04:11 +0100

    Update parte3.py

--- parte3.py
+++ parte3.py
@@ -5,6 +5,6 @@ import os
 
 os.system('rm -r practica_creativa2')
 
-os.system('git clone https://github.com/JaimeMadridAlonso/parte3.git')
+os.system('https://github.com/CDPS-ETSIT/practica_creativa2.git')
 
 os.system('cd practica_creativa2/bookinfo/src/reviews && docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
